281769bd4573498b33d5bde8be0354b40f9e104a
Nurbol Bektibay <nurbol.bektibay@intexsoft.kz>
2023-11-19 18:46:35 +0600

    Fixing migrations, docker-compose and changing response time histogram's buckets

--- cmd/api/metrics.go
+++ cmd/api/metrics.go
@@ -9,7 +9,7 @@ var (
 	responseTimeHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
 		Name:    "response_time_milliseconds",
 		Help:    "The histogram of response time in milliseconds",
-		Buckets: prometheus.DefBuckets,
+		Buckets: []float64{.5, 1, 2.5, 5, 10, 25, 50, 100, 150, 200},
 	})
 	processedRequestsCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "processed_requests_total",
